9e7c392f91f1573db72bddbb0b0bf3254486617a
Murat Tezyapar <murat.tezyapar@live.com>
2018-07-29 18:26:07 +0300

    initial commit.

---
+++ src/main/java/com/mojo/appversioncontrol/AppversioncontrolApplication.java
@@ -0,0 +1,12 @@
+package com.mojo.appversioncontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppversioncontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppversioncontrolApplication.class, args);
+	}
+}
---
+++ src/main/java/com/mojo/appversioncontrol/Test.java
@@ -0,0 +1,19 @@
+package com.mojo.appversioncontrol;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller    // This means that this class is a Controller
+@RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
+public class Test {
+
+    @GetMapping(path="/test") // Map ONLY GET Requests
+    public @ResponseBody String hello (@RequestParam String name) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+        return "Hello World!"+name;
+    }
+
+}
